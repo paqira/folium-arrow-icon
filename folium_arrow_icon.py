8464da5d350c8461d27e3d0802af913494981ae4
Kentaro Tatsumi <paqira.2019@gmail.com>
2024-12-03 00:37:28 +0900

    refactor

--- folium_arrow_icon.py
+++ folium_arrow_icon.py
@@ -8,7 +8,8 @@ from functools import cached_property
 from typing import TYPE_CHECKING, NamedTuple
 
 if TYPE_CHECKING:
-    from typing import Literal, Sequence
+    from typing import Literal
+    from collections.abc import Sequence
 
 import folium  # type: ignore
 
